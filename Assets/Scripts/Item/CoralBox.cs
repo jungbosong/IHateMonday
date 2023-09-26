fb4477216faefa0a75f268420b9191baacb8c354
AYJ12 <95719027+AYJ12@users.noreply.github.com>
2023-09-26 21:07:37 +0900

    아이템1

---
+++ Assets/Scripts/Item/CoralBox.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CoralBox : MonoBehaviour, IBox
+{
+    private ItemCreate create;
+    private bool needKey = true;
+    private int random;
+
+
+    private void Awake()
+    {
+        create = GetComponent<ItemCreate>();
+        random = Random.Range(0, 100);
+        if(random <= 30)    //키 필요 박스인지 설정
+            needKey = false;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            if (needKey)
+                OnKeyUsePopUpUI();
+            else
+                OnKeyUse();
+        }
+
+    }
+
+    public void OnKeyUsePopUpUI()
+    {
+        //키사용 박스 팝업 호출
+    }
+
+    public void OnKeyUse()  //박스 오픈
+    {
+        Managers.Destroy(this);
+        create.OnCreateItem();
+    }
+}
---
+++ Assets/Scripts/Item/ItemData.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Active,
+    Passive
+}
+
+public enum ConsumableType
+{
+    Hp,
+    Shield,
+    AttackPower,
+    AttackSpeed,
+    BulletAmount,
+    MoveSpeed,
+    IncreaseDamage,
+    BulletDelete,
+    Invincibility
+}
+
+[System.Serializable]
+public class ItemDataConsumable
+{
+    public ConsumableType type;
+    public float value;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("Info")]
+    public string itemName;
+    //public string toolTip;
+    public ItemType type;
+    public Sprite icon;
+
+    [Header("Stacking")]
+    public int stack;
+    public int maxStackAmount;
+
+    [Header("Consumable")]
+    public ItemDataConsumable[] consumables;
+}
