f35fe0ff259de1916f7eaa066a8ae18a705b6b85
Shyunju <hyunju9908@naver.com>
2023-09-26 14:28:32 +0900

    [ADD] 조준 시스템 추가(-신현주)
    Scrips/Player/CharacterAimRotation.cs

---
+++ Assets/Scripts/Player/CharacterAimRotation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownAimRotation : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer armRenderer;
+    [SerializeField] private Transform armPovot;
+    [SerializeField] private SpriteRenderer characterRenderer;
+    private CharacterController _controller;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+    void Start()
+    {
+        _controller.OnLookEvent += OnAim;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnAim(Vector2 newAimDirection)
+    {
+        RotateArm(newAimDirection);
+    }
+    private void RotateArm(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        armRenderer.flipY = Mathf.Abs(rotZ) > 90f;
+        characterRenderer.flipX = armRenderer.flipY;
+        armPovot.rotation = Quaternion.Euler(0, 0, rotZ);
+    }
+}
