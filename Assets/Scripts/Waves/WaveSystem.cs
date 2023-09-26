0a02c15e2a4f61ca7d0840eb3c80cb88ef8d2a1e
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-09-27 00:44:59 +0900

    [UPDATE] 웨이브 시스템 구조 변경 및 테스트 완료 (-정민)

--- Assets/Scripts/Waves/WaveSystem.cs
+++ Assets/Scripts/Waves/WaveSystem.cs
@@ -5,42 +5,27 @@ using UnityEngine;
 
 public class WaveSystem : MonoBehaviour
 {
-    const string WAVE_PREFAB_PATH = "Prefabs/Waves/Stage";
+    // wave 프리팹의 경로
+    const string WAVE_PREFAB_PATH = "Prefabs/Waves/Wave";
 
-    private string _dataPath;
-    private int _curStage;
-    private int _curRoomNum;
-    private bool _isBossRoom;
-
-    private List<GameObject> _waveList = new List<GameObject>();
-    private List<BossStage> _bossStageList;
+    private string _dataPath;   // 현재 스테이지 정보를 받아와서 완성하는 최종 프리팹 경로
+    private int _curStage;      // 현재 스테이지
+    private GameObject _wavePrefab;
 
     private void Start()
     {
-        _curStage = 1;
-        InitWaveList();
-        StartWave(1, false);
-    }
-
-    private void InitWaveList()
-    {
         _dataPath = WAVE_PREFAB_PATH + _curStage;
-        GameObject[] objs = Resources.LoadAll<GameObject>(_dataPath);
-
-        for (int i = 0; i < objs.Length; i++)
-        {
-            _waveList.Add(Instantiate(objs[i]));
-        }
+        _wavePrefab = Resources.Load<GameObject>(_dataPath);
     }
 
-    public void StartWave(int roomNum, bool isBossRoom)
+    public void StartWave(bool isBossRoom)
     {
         //if (isBossRoom)
         //{
-
+        //      
         //} else
         //{
-            _waveList[roomNum - 1].SetActive(true);
+        //  Instantiate(_wavePrefab);
         //}
     }
 }
