5220ef82702d4556b936164955d286c4638e6b64
Shyunju <hyunju9908@naver.com>
2023-09-26 16:45:40 +0900

    [ADD] 임시 gameover UI 추가(-현주)
    Scene/PlayerTest/endPanel

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,13 +6,40 @@ public class Player : MonoBehaviour
 {
     private Camera _mainCamera;
 
+    private HealthSystem _healthSystem;
+    [SerializeField] private SpriteRenderer _playerRenderer;
+    [SerializeField] private GameObject _endPanel;
+
+
     private void Awake()
     {
         _mainCamera = Camera.main;
+    }
+    private void Start()
+    {
+        _healthSystem = GetComponent<HealthSystem>();
+        _healthSystem.OnDamage += OnDamage;
+        _healthSystem.OnDeath += GameOver;
     }
 
     private void FixedUpdate()
     {
         _mainCamera.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+    }
+
+    void OnDamage()
+    {
+        _playerRenderer.color = new Color32(200, 100, 100, 255);
+        StartCoroutine(nameof(HitCo));
+    }
+    private IEnumerator HitCo()
+    {
+        yield return new WaitForSeconds(1f);
+        _playerRenderer.color = Color.white;
+    }
+
+    void GameOver()
+    {
+        _endPanel.SetActive(true);
     }
 }
