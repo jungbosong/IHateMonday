b58beecec44295f4b9b7deabe7710c04ced9d616
KimDaeMins <eoals81712@gmail.com>
2023-09-29 19:31:10 +0900

    [Update] Guns 수정
    [Update] 모든 총기류 몬스터도 사용 가능하게 수정.
    +풀테스트씬 머지하겠습니다.

--- Assets/Scripts/Gun/RailGun.cs
+++ Assets/Scripts/Gun/RailGun.cs
@@ -16,7 +16,7 @@ public class RailGun : CharzingGun
     protected override void Awake()
     {
         base.Awake();
-        tagetLayer = LayerMask.GetMask("Wall") | LayerMask.GetMask("Env") | LayerMask.GetMask("Monster");
+        tagetLayer = LayerMask.GetMask("Wall") | LayerMask.GetMask("Env") | LayerMask.GetMask("Monster") | LayerMask.GetMask("Player");
         _angle = (float[])_baseAngle.Clone();
         _lineRenderers = GetComponentsInChildren<LineRenderer>();
 
@@ -131,56 +131,6 @@ public class RailGun : CharzingGun
             lr.enabled = false;
         }
         _angle = (float[])_baseAngle.Clone();
-    }
-    public override void OnLook(Vector2 worldPos)
-    {
-        //플레이어의 트랜스폼을 받아온다 - 멤버변수로 두고 Awake에서 받아올예정
-        //플레이어에는 왼손좌표랑 오른손좌표도 필요하다.
-
-        Vector2 playerPosition = new Vector3(0, 0);
-        Vector2 playerLeftHandPosition = new Vector2(-0.3f, 0);
-        Vector2 playerRightHandPosition = new Vector2(0.3f, 0);
-
-        Vector2 leftdir;
-        Vector2 rightdir;
-
-        if ((playerPosition - worldPos).magnitude < (playerPosition - playerRightHandPosition).magnitude)
-            return;
-
-
-        leftdir = (worldPos - (Vector2)playerLeftHandPosition).normalized;
-        rightdir = (worldPos - (Vector2)playerRightHandPosition).normalized;
-
-
-
-        float leftRotZ = Mathf.Atan2(leftdir.y, leftdir.x) * Mathf.Rad2Deg;
-        float rightRotZ = Mathf.Atan2(rightdir.y, rightdir.x) * Mathf.Rad2Deg;
-
-        if (isLeftHand && Mathf.Abs(leftRotZ) < 75 && Mathf.Abs(rightRotZ) < 105)
-        {
-            isLeftHand = false;
-        }
-        else if (!isLeftHand && Mathf.Abs(rightRotZ) > 105 && Mathf.Abs(leftRotZ) > 75)
-        {
-            isLeftHand = true;
-        }
-
-        float rotY = 0f;
-        float rotZ = 0f;
-        if (isLeftHand)
-        {
-            transform.position = playerLeftHandPosition;
-            rotZ = 180f - Mathf.Atan2(leftdir.y, leftdir.x) * Mathf.Rad2Deg;
-            rotY = 180f;
-        }
-        else
-        {
-            transform.position = playerRightHandPosition;
-            rotZ = Mathf.Atan2(rightdir.y, rightdir.x) * Mathf.Rad2Deg;
-            rotY = 0;
-        }
-
-        transform.rotation = Quaternion.Euler(0, rotY, rotZ);
     }
 
     public override void OnRoll()
