9facee3a809faca59d9f5b16de1effd8a4ad1677
KimDaeMins <eoals81712@gmail.com>
2023-09-29 16:06:54 +0900

    [Add]BeziarCurve/UI_Interaction/Door.cs 추가
    [Add]UnityEngine/Asset/Script/UI/Util/BeziarCurve.cs 추가
    BeziarCurve : 베지어커브를 언젠간 쓸일이 있을것같아서 일단 올려둡니다..
    [Add]UnityEngine/Asset/Script/UI/UI_Popup/UI_Interaction.cs 추가
    UI_Interaction : 이벤트처리 위주로 하는 유아이를 하나 만들고싶었습니다. 사용법은 간단하니 쓸거면 함 물어봐주세요
    [Add]UnityEngine/Asset/Script/Objects(추가)/Door.cs 추가
    Obejcts : 오브젝트들을 여기에 옮겨놓으면 좋을 것 같습니다.
    Door : 문 미완성입니다 최신파일 받기위해 일단 넣겠습니다

---
+++ Assets/Scripts/UI/Util/BeziarCurve.cs
@@ -0,0 +1,89 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BeziarCurve
+{
+    private List<Vector3> _positions;
+    private List<Vector3> _rotations;
+    private List<Vector3> _scales;
+
+    public void InputTransform(Transform tr)
+    {
+        InputPosition(tr.position);
+        InputRotation(tr.rotation);
+        InputScale(tr.localScale);
+    }
+    public void InputPosition(Vector3 pos)
+    {
+        _positions.Add(pos);
+    }
+    public void InputRotation(Quaternion q)
+    {
+        _rotations.Add(q.eulerAngles);
+    }
+    public void InputRotation(Vector3 rot)
+    {
+        _rotations.Add(rot);
+    }
+    public void InputScale(Vector3 scale)
+    {
+        _scales.Add(scale);
+    }
+
+
+    //Èì... ¾Ö¸ÅÇÏ³×
+    public Vector3 GetBeziarPosition(float time)
+    {
+        Vector3[] positions = _positions.ToArray();
+        while(positions.Length != 1)
+        {
+            List<Vector3> nexts = new List<Vector3>();
+            for (int i = 0 ; i < positions.Length - 1 ; i++)
+            {
+                nexts.Add(Vector3.Lerp(positions[i], positions[i+1], time));
+            }
+
+            positions = nexts.ToArray();
+        }
+
+        return positions[0];
+    }
+
+    public Quaternion GetBeziarRotation(float time)
+    {
+        Vector3[] rotations = _rotations.ToArray();
+        while (rotations.Length != 1)
+        {
+            List<Vector3> nexts = new List<Vector3>();
+            for (int i = 0 ; i < rotations.Length - 1 ; i++)
+            {
+                nexts.Add(Vector3.Lerp(rotations[i] , rotations[i + 1] , time));
+            }
+
+            rotations = nexts.ToArray();
+        }
+
+        return Quaternion.Euler(rotations[0]);
+    }
+
+    public Vector3 GetBeziarScale(float time)
+    {
+        Vector3[] scales = _scales.ToArray();
+        while (scales.Length != 1)
+        {
+            List<Vector3> nexts = new List<Vector3>();
+            for (int i = 0 ; i < scales.Length - 1 ; i++)
+            {
+                nexts.Add(Vector3.Lerp(scales[i] , scales[i + 1] , time));
+            }
+
+            scales = nexts.ToArray();
+        }
+
+        return scales[0];
+    }
+
+}
