7d4c08510de7a7ccfdebb4f54286eb5da0ada291
Shyunju <hyunju9908@naver.com>
2023-10-02 20:20:16 +0900

    [CHORE] 일부 수정

--- Assets/Scripts/Scene/DeadScene.cs
+++ Assets/Scripts/Scene/DeadScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class DeadScene : BaseScene
+public class DeadScene : MonoBehaviour
 {
     public void LoadGameScene()
     {
